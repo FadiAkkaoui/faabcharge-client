15b4b52b26ad7312a38910b1e06fd641844af78a
Fadi Akkaoui <Fadi Akkaoui@DESKTOP-ATEABIL>
2021-04-28 15:17:28 +0200

    rt

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Navbar from './navbar';
 import Footer from './footer';
+import Login from './login';
 
 function App() {
   return (
     <div>
       <Navbar />
+
       <Footer/>
     </div>
   );
--- src/navbar.js
+++ src/navbar.js
@@ -17,38 +17,37 @@ function Navbar() {
                             <a className="nav-link active" aria-current="page" href="#"></a>
                         </li>
                         <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Components</a>
+                            <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Configurations</a>
                             <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                <li><a className="dropdown-item" href="#">All Configurations</a></li>
+                                <li><a className="dropdown-item" href="#">New Configurations</a></li>
                                 <li><hr className="dropdown-divider"></hr></li>
-                                <li><a className="dropdown-item" href="#">Separated link</a></li>
+                                <li><a className="dropdown-item" href="#">Compare Configurations</a></li>
                             </ul>
                         </li>
                         <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Items</a>
                             <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                <li><a className="dropdown-item" href="#">All Items</a></li>
+                                <li><a className="dropdown-item" href="#">New Items</a></li>
                                 <li><hr className="dropdown-divider"></hr></li>
-                                <li><a className="dropdown-item" href="#">Separated link</a></li>
+                                <li><a className="dropdown-item" href="#">Compare Items</a></li>
                             </ul>
                         </li>
                         <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Configurations</a>
+                            <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Components</a>
+                            <div className="dropdown1"></div>
                             <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                <li><a className="dropdown-item" href="#">All Components</a></li>
+                                <li><a className="dropdown-item" href="#">New Components</a></li>
+                                <li><a className="dropdown-item" href="#"></a></li>
                                 <li><hr className="dropdown-divider"></hr></li>
-                                <li><a className="dropdown-item" href="#">Separated link</a></li>
+                                <li><a className="dropdown-item" href="#"></a></li>
                             </ul>
                         </li>
                     </ul>
                     <div className="nav-item" id="itemsb">
-                            <a className="nav-link active" aria-current="page" href="#">Log out</a>
+                            <button className="button1"><p className="nav-link active" aria-current="page" href="#">Log out</p></button>
                         </div>
                 </div>
             </div>
